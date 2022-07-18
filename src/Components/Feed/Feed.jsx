7b2e324bfa15a4de4de6601bfce855eefb4bff96
Gian Manzo <ggianmanzo@gmail.com>
2022-07-18 10:03:19 -0300

    configure redux feed photos

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { FeedModal } from './FeedModal'
 import { FeedPhotos } from './FeedPhotos'
 import PropTypes from 'prop-types'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadNewPhoto, resetFeedState } from '../../store/feed'
+import { Loading } from '../Helper/Loading'
+import { Error } from '../Helper/Error'
 
 export const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null)
-  const [pages, setPages] = React.useState([1])
-  const [infinite, setInfinite] = React.useState(true)
+  const { infinite, loading, list, error } = useSelector(state => state.feed)
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    dispatch(resetFeedState())
+    dispatch(loadNewPhoto({ user, total: 6 }))
+  }, [dispatch, user])
 
   React.useEffect(() => {
     let wait = false
@@ -15,7 +24,7 @@ export const Feed = ({ user }) => {
         const scroll = window.scrollY
         const height = document.body.offsetHeight - window.innerHeight
         if (scroll > height * 0.75 && !wait) {
-          setPages(pages => [...pages, pages.length + 1])
+          dispatch(loadNewPhoto({ user, total: 6 }))
           wait = true
           setTimeout(() => (wait = false), 500)
         }
@@ -29,22 +38,16 @@ export const Feed = ({ user }) => {
       window.removeEventListener('wheel', infiniteScroll)
       window.removeEventListener('scroll', infiniteScroll)
     }
-  }, [infinite])
+  }, [infinite, dispatch, user])
 
   return (
     <div>
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      {pages.map(page => (
-        <FeedPhotos
-          key={page}
-          user={user}
-          page={page}
-          setModalPhoto={setModalPhoto}
-          setInfinite={setInfinite}
-        />
-      ))}
+      {list.length > 0 && <FeedPhotos setModalPhoto={setModalPhoto} />}
+      {loading && <Loading />}
+      {error && <Error error={error} />}
     </div>
   )
 }
--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { FeedPhotoItem } from './FeedPhotoItem'
-import { useFetch } from '../../Hooks/useFetch'
-import { PHOTOS_GET } from '../../api'
-import { Error } from '../Helper/Error'
-import { Loading } from '../Helper/Loading'
 
 const PhotosUl = styled.ul`
   display: grid;
@@ -18,31 +15,18 @@ const PhotosUl = styled.ul`
   }
 `
 
-export const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
-  const { data, loading, error, request } = useFetch()
+export const FeedPhotos = ({ setModalPhoto }) => {
+  const { list } = useSelector(state => state.feed)
 
-  React.useEffect(() => {
-    async function fetchPhotos() {
-      const total = 3
-      const { url, options } = PHOTOS_GET({ page, total, user })
-      const { response, json } = await request(url, options)
-      if (response && response.ok && json.length < total) setInfinite(false)
-    }
-    fetchPhotos()
-  }, [request, user, page, setInfinite])
-  if (error) return <Error error={error} />
-  if (loading) return <Loading />
-  if (data)
-    return (
-      <PhotosUl className="animeLeft">
-        {data.map(photo => (
-          <FeedPhotoItem
-            key={photo.id}
-            photo={photo}
-            setModalPhoto={setModalPhoto}
-          />
-        ))}
-      </PhotosUl>
-    )
-  else return null
+  return (
+    <PhotosUl className="animeLeft">
+      {list.map(photo => (
+        <FeedPhotoItem
+          key={photo.id}
+          photo={photo}
+          setModalPhoto={setModalPhoto}
+        />
+      ))}
+    </PhotosUl>
+  )
 }
